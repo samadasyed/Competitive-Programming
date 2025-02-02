89a0c27302037f4f48a6a517f1be908f59a0f946
samadasyed <samadasyed@hotmail.com>
2025-02-02 09:14:37 -0600

    Final Submission

---
+++ TopKFrequentElements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        std::unordered_map<int, int> freq; 
+        
+       for (int x : nums) {
+        freq[x]++; 
+       }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
+
+        for (auto &p : freq) {
+            pq.push({p.second, p.first}); 
+            if ((int) pq.size() > k) {
+                pq.pop(); 
+            }
+        }
+        vector<int> res;
+        while (!pq.empty()) {
+            res.push_back(pq.top().second); 
+            pq.pop(); 
+            } 
+        return res; 
+    }
+
+};
