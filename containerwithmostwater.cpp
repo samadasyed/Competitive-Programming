b1a240d45c0cddd19a318619cdb2d4e5d61f38ad
samadasyed <samadasyed@hotmail.com>
2025-03-04 13:55:49 -0600

    Final Submission

---
+++ containerwithmostwater.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0; 
+        int right = height.size()-1;
+        int maxArea = 0; 
+        while (left < right) {
+            int temp = min(height[left],height[right]) * (right-left); 
+            if (temp > maxArea) maxArea = temp; 
+
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+};
