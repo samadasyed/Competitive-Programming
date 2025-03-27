0ec6ca9467abf6b75055066ce4b98cc61c9d8528
samadasyed <samadasyed@hotmail.com>
2025-03-26 19:47:01 -0500

    Final Submission i think

---
+++ twosumii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int s = 0; 
+        int b = numbers.size()-1;
+
+        while (s < b) {
+            int two_sum = numbers[s] + numbers[b];
+            if (two_sum > target){
+                b--; 
+            }
+
+            if (two_sum < target){ 
+                s++; 
+            }
+            if (two_sum == target) {
+                return {s+1,b+1}; 
+            }
+        }
+        return {};
+    }
+};
