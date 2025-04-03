01f7df5c408d09a567476a0ce6ab175deaa295cd
samadasyed <samadasyed@hotmail.com>
2025-04-03 11:18:27 -0500

    final

--- movezeros.cpp
+++ movezeros.cpp
@@ -2,12 +2,22 @@
 
 using namespace std; 
 
-
-
-
-
-
-
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        //two pointers in the same direction
+      int slow = 0; 
+      for (int fast = 0; fast < nums.size(); fast++) {
+        if (nums[fast] != 0){
+            int slow_num = nums[slow]; //these three lines are a swap
+            nums[slow] = nums[fast];
+            nums[fast] = slow_num; 
+            slow++; 
+        }
+      }
+        
+    }
+};
 
 
 int main () {
