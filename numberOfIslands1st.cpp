c00054142a5102cf70a7e6f7912f0797331a628f
samadasyed <samadasyed@hotmail.com>
2026-01-04 17:00:16 -0600

    Retrying numofIslands

--- numberOfIslands1st.cpp
+++ numberOfIslands1st.cpp
@@ -1,26 +1,11 @@
 #include <bits/stdc++.h> 
 using namespace std; 
 
-
+int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}}; //up, down, right, left
 
 int numIslands(vector<vector<char>>&grid) {
 
-    if (grid.empty()) return 0; 
-
-    int numOfIslands; 
-    int r = 0, c = 0; 
-    auto initial = grid[r][c];
-
-    if (initial == 1) {
-        numOfIslands++; 
-
-        
-    }
-
-    auto right = grid[r][c+1]; 
-    auto left = grid[r][c-1]; 
-    auto up = grid [r-1][c];
-    auto down = grid[r+1][c]; 
+  
 
 }
 
