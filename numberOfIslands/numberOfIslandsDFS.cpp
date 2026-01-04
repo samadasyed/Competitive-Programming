e708a5a3b745c1fa63d99e0ac3d52b9b70ea2573
samadasyed <samadasyed@hotmail.com>
2026-01-04 17:06:10 -0600

    Making different file for DFS implementation

---
+++ numberOfIslands/numberOfIslandsDFS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int directions[4][2] = {{1,0}, {-1,0}, {0,1}, {0,-1}}; //up, down, right, left
+
+int numIslands(vector<vector<char>>&grid) {
+    
+  
+
+}
+
+
+int main () {
+
+   vector<vector<char>> gridExample1 = {
+    {'1','1','1','1','0'},
+    {'1','1','0','1','0'},
+    {'1','1','0','0','0'},
+    {'0','0','0','0','0'}
+    }; 
+
+
+    vector<vector<char>> gridExample2 = {
+    {'1','1','0','0','0'},
+    {'1','1','0','0','0'},
+    {'0','0','1','0','0'},
+    {'0','0','0','1','1'}
+    }; 
+
+    return 0; 
+}
