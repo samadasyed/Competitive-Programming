e82cc3913091721572c8cdf442620e1ef2858bdd
samadasyed <samadasyed@hotmail.com>
2026-01-08 18:24:11 -0600

    Filled in rest

--- CloneGraph/cloneGraphDFS.cpp
+++ CloneGraph/cloneGraphDFS.cpp
@@ -15,7 +15,26 @@ public:
 // -------------------- Your Solution (NO implementation here) --------------------
 class Solution {
 public:
-    Node* cloneGraph(Node* node); // implement this in your IDE
+    Node* cloneGraph(Node* node) {
+        map<Node*, Node*> oldToNew;
+        return dfs(node, oldToNew); 
+    }
+    Node* dfs (Node* node, map<Node*, Node*>& oldToNew) {
+        if (node == nullptr) {
+            return nullptr; 
+        }
+        if (oldToNew.count(node)) {
+            return oldToNew[node]; 
+        }
+
+        Node* copy  = new Node(node->val); 
+        oldToNew[node] = copy; 
+
+        for (Node* nei : node->neighbors) {
+            copy->neighbors.push_back(dfs(nei, oldToNew)); 
+        }
+        return copy; 
+    }
 };
 
 // -------------------- Parsing helpers --------------------
