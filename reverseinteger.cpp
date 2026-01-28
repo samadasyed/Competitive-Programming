30a6d295db19887eb9c968830520a9d1074a7313
samadasyed <samadasyed@hotmail.com>
2026-01-28 12:32:23 -0600

    Did the BF but had to hardcode the exception at 32 bits upper limit

--- reverseinteger.cpp
+++ reverseinteger.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int reverse(int x) {
-        vector<char> lol; 
-        int xlength = to_string(abs(x)).length();
-        string s = to_string(x);
-
-        for (int i = xlength-1; i >= 0; i--){
-            lol.push_back(s[i]); 
+        int original = x; 
+        if (x == -2147483648) {
+            return 0; 
         }
-        int y = 0;
-        for (char c : lol) {
-            y = y * 10 + (c - '0');
+        x = abs(x); 
+        string s = to_string(x); 
+        std::reverse(s.begin(), s.end()); 
+        long long res = stoll(s);
+        if (original < 0) {
+              res *= -1;
         }
-        return y;
+        if (res < -(1LL << 31) || res > (1LL << 31) - 1) {
+            return 0;
+        }
+        return static_cast<int>(res);
     }
 };
